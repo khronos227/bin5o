79b68e837b0c7c17c12e8ee301dbf8342fe7af01
takefumi.ota <takefumi.ota@mail.rakuten.com>
2014-09-06 01:24:25 +0900

    [make-app] favicon呼び出しとタイトル変更

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'bin5o' });
 });
 
 module.exports = router;
